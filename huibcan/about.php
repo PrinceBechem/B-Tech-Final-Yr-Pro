2592ab216f7541de4eaf33516d5f007ebe457aea
PrinceBechem <mbubechem@gmail.com>
2024-08-27 07:22:11 +0100

    initial commit

---
+++ huibcan/about.php
@@ -0,0 +1,44 @@
+<?php include('./constant/layout/head.php');?>
+<?php include('./constant/layout/header.php');?>
+
+<?php include('./constant/layout/sidebar.php');?>
+
+
+<div class="page-wrapper">
+	<div class="container-fluid">
+		<div class="card">
+			<div class="card-body">
+				<div class="about-head text-center mb-5">
+					<h1 style="color: black;" class="mb-2 mt-4 f-s-50 ">About Mbu Behem.</h1>
+					<span> Web Developer</span>
+				</div>
+				<div class="about">
+					<div class="row">
+						<div class="col-md-3">
+							<img src="assets/myimages/Maleprofile.png" class="img-fluid">
+						</div>
+						
+						<div class="col-md-5">
+							<h3 class="mb-1" style="text-align: justify; color: gray;">A Computer Science(SWE) Degree graduate, keen on stirring up creative mind and information into PHP project ideas and working as a freelancer for Software projects. He also work in Python, Codeignitor and Laravel. Prince B likes to learn new things related to her profession.</h3>
+							<h3 style="text-align: justify; color: gray;" >Soon He is going to launch an online course on Software projects for beginners. You can contact Him for any Project development, either academic or commercial Projects.</h3>
+							<h3 style="text-align: justify;"><b><mark>For students or anyone else who needs program or any Professional Software Development, Website Development, at affordable cost contact at mbubechem@gmail.com</mark></b></h3>
+						</div>
+						<div class="col-md-4">
+							<ul class="list-unstyled">
+								<li><span>Work with</span> : <b>HTML, PHP, MySQL, Laravel, Python</b></li>
+								<li><span>Exprience</span> :  <b>3 Years </b></li>
+								<li><span>Residence</span> :  <b>Cameroon, Buea </b></li>
+								<li><span>Email</span> :  <b>mbubechem@gmail.com</b></li>
+								<li><span>Freelancer</span> :  <b>Available</b></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>			
+		</div>
+	</div>
+</div>
+
+
+<?php include('./constant/layout/footer.php');?>
